7aecd5fb90168537b023fa106e1560d934741aca
Xavier Villelégier <x.villelegier@gmail.com>
2018-01-28 17:00:02 +0100

    Add a LinearGradient example in button_home (#36)
    * Add a LinearGradient example in button_home
    
    * Update submodule

--- src/views/login/screen2.js
+++ src/views/login/screen2.js
@@ -244,6 +244,7 @@ export default class LoginScreen3 extends Component {
               text="SIGNUP"
               containerStyle={{ flex: -1 }}
               buttonStyle={styles.signUpButton}
+              ButtonComponent={require('expo').LinearGradient}
               linearGradientProps={{
                 colors: ['#FF9800', '#F44336'],
                 start: [1, 0],

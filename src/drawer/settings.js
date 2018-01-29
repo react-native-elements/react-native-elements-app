e1295e5bd8b6610555c9d4256b8a795422014729
Guizmo Martinez <martinezguillaume@users.noreply.github.com>
2018-01-29 14:40:49 +0100

    Add settings page

---
+++ src/drawer/settings.js
@@ -0,0 +1,42 @@
+import Expo from 'expo';
+import React from 'react';
+import { StackNavigator } from 'react-navigation';
+import { Icon } from 'react-native-elements';
+
+import Settings from '../views/settings';
+
+const SettingsDrawerItem = StackNavigator({
+  Settings: {
+    screen: Settings,
+    navigationOptions: ({ navigation }) => ({
+      title: 'Settings',
+      headerLeft: (
+        <Icon
+          name="menu"
+          size={30}
+          type="entypo"
+          iconStyle={{ paddingLeft: 10 }}
+          onPress={() => navigation.navigate('DrawerOpen')}
+        />
+      ),
+    }),
+  },
+});
+
+SettingsDrawerItem.navigationOptions = {
+  drawerLabel: 'Settings',
+  drawerIcon: ({ tintColor }) => (
+    <Icon
+      name="cog"
+      size={30}
+      iconStyle={{
+        width: 30,
+        height: 30,
+      }}
+      type="entypo"
+      color={tintColor}
+    />
+  ),
+};
+
+export default SettingsDrawerItem;

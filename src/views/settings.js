23559e68d3492e4fc5d6a59d5e73dc6cb3684fd1
Guizmo Martinez <martinezguillaume@users.noreply.github.com>
2018-01-29 15:36:27 +0100

    Add more icons ❤️

--- src/views/settings.js
+++ src/views/settings.js
@@ -3,40 +3,113 @@ import { View, StyleSheet, SectionList } from 'react-native';
 
 import { ListItem, Divider } from 'react-native-elements';
 
+const ORANGE = '#FF9500';
+const BLUE = '#007AFF';
+const GREEN = '#4CD964';
+const RED = '#FF3B30';
+const GREY = '#8E8E93';
+const PURPLE = '#5856D6';
+const TEAL_BLUE = '#5AC8FA';
+
 const sections = [
   {
     data: [
       {
         title: 'Airplane Mode',
         icon: 'ios-plane',
-        backgroundColor: '#f09628',
+        backgroundColor: ORANGE,
         hideDisclosure: true,
         checkbox: true,
       },
       {
         title: 'Wi-Fi',
-        backgroundColor: '#147dfa',
+        backgroundColor: BLUE,
         icon: 'ios-wifi',
       },
       {
         title: 'Bluetooth',
-        backgroundColor: '#147dfa',
+        backgroundColor: BLUE,
         icon: 'ios-bluetooth',
         rightTitle: 'Off',
       },
       {
         title: 'Cellular',
-        backgroundColor: '#147dfa',
-        icon: 'ios-bluetooth',
+        backgroundColor: GREEN,
+        icon: 'ios-phone-portrait',
       },
       {
         title: 'Personal Hotspot',
-        backgroundColor: '#50dc64',
-        icon: 'ios-bluetooth',
+        backgroundColor: GREEN,
+        icon: 'ios-radio-outline',
         rightTitle: 'Off',
       },
     ],
   },
+  {
+    data: [
+      {
+        title: 'Notifications',
+        icon: 'ios-notifications',
+        backgroundColor: RED,
+      },
+      {
+        title: 'Control Center',
+        backgroundColor: GREY,
+        icon: 'ios-switch',
+      },
+      {
+        title: 'Do Not Disturb',
+        backgroundColor: PURPLE,
+        icon: 'ios-moon',
+      },
+    ],
+  },
+  {
+    data: [
+      {
+        title: 'General',
+        icon: 'ios-settings',
+        backgroundColor: GREY,
+      },
+      {
+        title: 'Display & Brightness',
+        backgroundColor: BLUE,
+        icon: 'ios-bulb',
+      },
+      {
+        title: 'Wallpaper',
+        backgroundColor: TEAL_BLUE,
+        icon: 'ios-color-wand',
+      },
+      {
+        title: 'Sounds',
+        backgroundColor: RED,
+        icon: 'ios-volume-up',
+      },
+      {
+        title: 'Touch ID & Code',
+        backgroundColor: RED,
+        icon: 'ios-finger-print',
+      },
+      {
+        title: 'Emergency Call',
+        backgroundColor: ORANGE,
+        icon: 'ios-medical',
+      },
+      {
+        title: 'Battery',
+        backgroundColor: GREEN,
+        icon: 'ios-battery-full',
+      },
+      {
+        title: 'Confidentiality',
+        backgroundColor: GREY,
+        icon: 'ios-hand',
+      },
+    ],
+  },
+  // Space at the bottom
+  { data: [] },
 ];
 
 export default class Settings extends React.PureComponent {
@@ -60,29 +133,27 @@ export default class Settings extends React.PureComponent {
     />
   );
 
-  renderSectionHeader = () => <View style={{ height: 40 }} />;
+  renderSectionHeader = () => <View style={{ height: 30 }} />;
 
   ItemSeparatorComponent = () => <Divider style={{ marginLeft: 58 }} />;
 
   render() {
     return (
-      <View style={styles.container}>
-        <SectionList
-          sections={sections}
-          renderItem={this.renderItem}
-          renderSectionHeader={this.renderSectionHeader}
-          ItemSeparatorComponent={this.ItemSeparatorComponent}
-          SectionSeparatorComponent={Divider}
-          stickySectionHeadersEnabled={false}
-        />
-      </View>
+      <SectionList
+        contentContainerStyle={styles.containerStyle}
+        sections={sections}
+        renderItem={this.renderItem}
+        renderSectionHeader={this.renderSectionHeader}
+        ItemSeparatorComponent={this.ItemSeparatorComponent}
+        SectionSeparatorComponent={Divider}
+        stickySectionHeadersEnabled={false}
+      />
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#EFEFF4',
   },
 });

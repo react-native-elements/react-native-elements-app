e6071812262181fc3c88bf4de8f05e39707f0e9c
React Native Elements CI <x.villelegier@gmail.com>
2018-04-01 13:31:08 +0200

    Update Buttons to new layout

--- src/views/buttons_home.js
+++ src/views/buttons_home.js
@@ -23,156 +23,155 @@ class Buttons extends Component {
             <Icon color="white" name="rocket" size={62} />
             <Text style={styles.heading}>Buttons</Text>
           </View>
-          <Button containerStyle={{marginVertical: 10}} />
-          <Button
-            title='LOG IN'
-            buttonStyle={{height: 50, width: 250, backgroundColor: 'black', borderWidth: 2, borderColor: 'white', borderRadius: 30}}
-            containerStyle={{marginVertical: 10}}
-            titleStyle={{fontWeight: 'bold'}}
-          />
-          <Button
-            title='Log in'
-            loading={false}
-            loadingProps={{size: 'small', color: 'white'}}
-            buttonStyle={{height: 50, width: 230, backgroundColor: 'rgba(111, 202, 186, 1)', borderRadius: 5}}
-            titleStyle={{fontWeight: 'bold', fontSize: 23}}
-            containerStyle={{marginVertical: 10}}
-            onPress={() => console.log('aye')}
-            underlayColor="transparent"
-          />
-          <Button
-            title="Add to Cart"
-            titleStyle={{fontWeight: 'bold', fontSize: 18}}
-            linearGradientProps={{
-              colors: ['#FF9800', '#F44336'],
-              start: [1, 0],
-              end: [0.2, 0],
-            }}
-            buttonStyle={{height: 40, width: 200, borderWidth: 0, borderColor: 'transparent', borderRadius: 20}}
-            containerStyle={{marginVertical: 10}}
-            icon={
-              <Icon
-                name='arrow-right'
-                size={15}
-                color='white'
-              />
-            }
-            iconRight
-            iconContainerStyle={{marginLeft: 5}}
-          />
-          <Button
-            title="Request an agent"
-            titleStyle={{fontWeight: '500'}}
-            buttonStyle={{backgroundColor: 'rgba(199, 43, 98, 1)', width: 275, height: 45, borderColor: 'transparent', borderWidth: 0}}
-            containerStyle={{marginTop: 10}}
-          />
-          <Button
-            title="SIGN UP"
-            disabled={true}
-            titleStyle={{fontWeight: '700'}}
-            buttonStyle={{backgroundColor: 'rgba(92, 99,216, 1)', width: 300, height: 45, borderColor: 'transparent', borderWidth: 0, borderRadius: 5}}
-            containerStyle={{marginTop: 20}}
-          />
-          <Button
-            title="SIGN UP"
-            loading={true}
-            loadingProps={{size: 'large', color: 'rgba(111, 202, 186, 1)'}}
-            titleStyle={{fontWeight: '700'}}
-            buttonStyle={{backgroundColor: 'rgba(92, 99,216, 1)', width: 300, height: 45, borderColor: 'transparent', borderWidth: 0, borderRadius: 5}}
-            containerStyle={{marginTop: 20}}
-          />
-          <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
+          <View style={{alignItems: 'center'}}>
+            <Button containerStyle={{marginVertical: 10}} />
             <Button
-              title="HOME"
-              icon={
-                <Icon
-                  name='home'
-                  size={15}
-                  color='white'
-                />
-              }
-              iconContainerStyle={{marginRight: 10}}
-              titleStyle={{fontWeight: '700'}}
-              buttonStyle={{backgroundColor: 'rgba(90, 154, 230, 1)', width: 130, borderColor: 'transparent', borderWidth: 0, borderRadius: 30}}
-              containerStyle={{marginTop: 20}}
+              title='LOG IN'
+              buttonStyle={{backgroundColor: 'black', borderWidth: 2, borderColor: 'white', borderRadius: 30}}
+              containerStyle={{marginVertical: 10, height: 50, width: 250 }}
+              titleStyle={{fontWeight: 'bold'}}
             />
             <Button
-              title="PROFILE"
+              title='Log in'
+              loading={false}
+              loadingProps={{size: 'small', color: 'white'}}
+              buttonStyle={{backgroundColor: 'rgba(111, 202, 186, 1)', borderRadius: 5}}
+              titleStyle={{fontWeight: 'bold', fontSize: 23}}
+              containerStyle={{marginVertical: 10, height: 50, width: 230}}
+              onPress={() => console.log('aye')}
+              underlayColor="transparent"
+            />
+            <Button
+              title="Add to Cart"
+              titleStyle={{fontWeight: 'bold', fontSize: 18}}
+              linearGradientProps={{
+                colors: ['#FF9800', '#F44336'],
+                start: [1, 0],
+                end: [0.2, 0],
+              }}
+              buttonStyle={{borderWidth: 0, borderColor: 'transparent', borderRadius: 20}}
+              containerStyle={{marginVertical: 10, height: 40, width: 200}}
               icon={
                 <Icon
-                  name='user'
+                  name='arrow-right'
                   size={15}
                   color='white'
                 />
               }
               iconRight
-              iconContainerStyle={{marginLeft: 10}}
+              iconContainerStyle={{marginLeft: 5}}
+            />
+            <Button
+              title="Request an agent"
+              titleStyle={{fontWeight: '500'}}
+              buttonStyle={{backgroundColor: 'rgba(199, 43, 98, 1)', borderColor: 'transparent', borderWidth: 0}}
+              containerStyle={{marginTop: 10, width: 275, height: 45}}
+            />
+            <Button
+              title="SIGN UP"
+              disabled={true}
               titleStyle={{fontWeight: '700'}}
-              buttonStyle={{backgroundColor: 'rgba(199, 43, 98, 1)', width: 150, borderColor: 'transparent', borderWidth: 0, borderRadius: 30}}
-              containerStyle={{marginTop: 20}}
-            />
-          </View>
-          <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
-            <Button
-              title="Basic Button"
-              buttonStyle={{backgroundColor: 'rgba(78, 116, 289, 1)', borderRadius: 3}}
-              containerStyle={{marginTop: 20}}
+              buttonStyle={{backgroundColor: 'rgba(92, 99,216, 1)', borderColor: 'transparent', borderWidth: 0, borderRadius: 5}}
+              containerStyle={{marginTop: 20, width: 300, height: 45}}
             />
             <Button
-              title="Outline Button"
-              buttonStyle={{backgroundColor: 'white', borderColor: 'rgba(78, 116, 289, 1)', borderWidth: 1}}
-              containerStyle={{marginTop: 20}}
-              titleStyle={{color: 'rgba(78, 116, 289, 1)'}}
-            />
-          </View>
-          <View style={{justifyContent: 'space-around', alignItems: 'center', flexDirection: 'row'}}>
-            <Button
-              title="HOME"
+              title="SIGN UP"
               loading={true}
+              loadingProps={{size: 'large', color: 'rgba(111, 202, 186, 1)'}}
               titleStyle={{fontWeight: '700'}}
-              buttonStyle={{backgroundColor: 'rgba(111, 202, 186, 1)', width: 100, height: 30, borderColor: 'transparent', borderWidth: 0, borderRadius: 30}}
-              containerStyle={{marginTop: 20}}
+              buttonStyle={{backgroundColor: 'rgba(92, 99,216, 1)', borderColor: 'transparent', borderWidth: 0, borderRadius: 5, paddingVertical: 10}}
+              containerStyle={{marginTop: 20, width: 300, height: 45}}
             />
-            <Button
-              title="Clear Button"
-              clear
-              titleStyle={{color: 'rgba(78, 116, 289, 1)'}}
-              containerStyle={{marginTop: 20}}
-            />
-          </View>
-          <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
-            <Button
-              title="Light"
-              buttonStyle={{backgroundColor: 'rgba(244, 244, 244, 1)', width: null, height: 40, borderRadius: 3}}
-              containerStyle={{marginTop: 20}}
-              titleStyle={{marginHorizontal: 20, color: 'black'}}
-            />
-            <Button
-              title="Dark"
-              buttonStyle={{backgroundColor: 'rgba(39, 39, 39, 1)', width: null, height: 40}}
-              containerStyle={{marginTop: 20}}
-              titleStyle={{color: 'white', marginHorizontal: 20}}
-            />
-            <Button
-              title="Default"
-              containerStyle={{marginTop: 20}}
-              buttonStyle={{backgroundColor: 'rgba(78, 116, 289, 1)', width: null, height: 40}}
-              titleStyle={{color: 'white', marginHorizontal: 20}}
-            />
-          </View>
-          <View style={{flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center', marginBottom: 20}}>
-            <Button
-              title="Secondary"
-              buttonStyle={{backgroundColor: 'rgba(127, 220, 103, 1)', width: null, height: 40}}
-              containerStyle={{marginTop: 20}}
-              titleStyle={{color: 'white', marginHorizontal: 20}}
-            />
-            <Button
-              title="Danger"
-              buttonStyle={{backgroundColor: 'rgba(214, 61, 57, 1)', width: null, height: 40}}
-              containerStyle={{marginTop: 20}}
-              titleStyle={{color: 'white', marginHorizontal: 20}}
-            />
+            <View style={styles.buttonsContainer}>
+              <Button
+                title="HOME"
+                icon={
+                  <Icon
+                    name='home'
+                    size={15}
+                    color='white'
+                  />
+                }
+                iconContainerStyle={{marginRight: 10}}
+                titleStyle={{fontWeight: '700'}}
+                buttonStyle={{backgroundColor: 'rgba(90, 154, 230, 1)', borderColor: 'transparent', borderWidth: 0, borderRadius: 30}}
+                containerStyle={{width: 130}}
+              />
+              <Button
+                title="PROFILE"
+                icon={
+                  <Icon
+                    name='user'
+                    size={15}
+                    color='white'
+                  />
+                }
+                iconRight
+                iconContainerStyle={{marginLeft: 10}}
+                titleStyle={{fontWeight: '700'}}
+                buttonStyle={{backgroundColor: 'rgba(199, 43, 98, 1)', borderColor: 'transparent', borderWidth: 0, borderRadius: 30}}
+                containerStyle={{width: 150}}
+              />
+            </View>
+            <View style={styles.buttonsContainer}>
+              <Button
+                title="Basic Button"
+                buttonStyle={{backgroundColor: 'rgba(78, 116, 289, 1)', borderRadius: 3}}
+              />
+              <Button
+                title="Outline Button"
+                buttonStyle={{backgroundColor: 'white', borderColor: 'rgba(78, 116, 289, 1)', borderWidth: 1}}
+                titleStyle={{color: 'rgba(78, 116, 289, 1)'}}
+              />
+            </View>
+            <View style={styles.buttonsContainer}>
+              <Button
+                title="HOME"
+                loading
+                titleStyle={{fontWeight: '700'}}
+                buttonStyle={{backgroundColor: 'rgba(111, 202, 186, 1)', borderColor: 'transparent', borderWidth: 0, borderRadius: 30, paddingVertical: 10}}
+                containerStyle={{width: 100, height: 40}}
+              />
+              <Button
+                title="Clear Button"
+                clear
+                titleStyle={{color: 'rgba(78, 116, 289, 1)'}}
+              />
+            </View>
+            <View style={styles.buttonsContainer}>
+              <Button
+                title="Light"
+                buttonStyle={{backgroundColor: 'rgba(244, 244, 244, 1)', borderRadius: 3}}
+                containerStyle={{height: 40}}
+                titleStyle={{marginHorizontal: 20, color: 'black'}}
+              />
+              <Button
+                title="Dark"
+                buttonStyle={{backgroundColor: 'rgba(39, 39, 39, 1)'}}
+                containerStyle={{height: 40}}
+                titleStyle={{color: 'white', marginHorizontal: 20}}
+              />
+              <Button
+                title="Default"
+                containerStyle={{height: 40}}
+                buttonStyle={{backgroundColor: 'rgba(78, 116, 289, 1)'}}
+                titleStyle={{color: 'white', marginHorizontal: 20}}
+              />
+            </View>
+            <View style={[styles.buttonsContainer, { marginBottom: 20 }]}>
+              <Button
+                title="Secondary"
+                buttonStyle={{backgroundColor: 'rgba(127, 220, 103, 1)'}}
+                containerStyle={{height: 40}}
+                titleStyle={{color: 'white', marginHorizontal: 20}}
+              />
+              <Button
+                title="Danger"
+                buttonStyle={{backgroundColor: 'rgba(214, 61, 57, 1)'}}
+                containerStyle={{height: 40}}
+                titleStyle={{color: 'white', marginHorizontal: 20}}
+              />
+            </View>
           </View>
           <View style={[styles.headerContainer, { backgroundColor: '#292C44'}]}>
             <Icon color="white" name="wrench" size={62} />
@@ -206,7 +205,13 @@ const styles = StyleSheet.create({
     backgroundColor: 'white'
   },
   contentView: {
-    flex: 1
+    flex: 1,
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+    marginTop: 20,
   },
   headerContainer: {
     justifyContent: 'center',

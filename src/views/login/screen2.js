04a014e3414c10286a818ee98acd9208f540fbdf
Ruslan Baybekov <oxyii.dev@gmail.com>
2019-03-07 19:46:30 +0300

    fix regexp eslint warnings

--- src/views/login/screen2.js
+++ src/views/login/screen2.js
@@ -95,7 +95,7 @@ export default class LoginScreen3 extends Component {
 
   validateEmail() {
     const { email } = this.state;
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     const emailValid = re.test(email);
     LayoutAnimation.easeInEaseOut();
     this.setState({ emailValid });

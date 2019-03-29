9bb38d2cb9519a950715b8ed14d5e60c26d04ae3
Monte Thakkar <manthan.thakkar@gmail.com>
2019-03-28 22:46:54 -0700

    Updates

---
+++ precache-manifest.5c17715f8a0fb168931cf941589e7261.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "2759de5c01527bd9730b4d1838e6c938",
+    "url": "/react-native-elements-app/static/media/Ubuntu-Light.2759de5c.ttf"
+  },
+  {
+    "revision": "7964ebb14a69a7093a0b",
+    "url": "/react-native-elements-app/static/js/main.e17011c9.chunk.js"
+  },
+  {
+    "revision": "d15c1216957060fac577af6151fb8cfe",
+    "url": "/react-native-elements-app/static/media/MaterialCommunityIcons.d15c1216.ttf"
+  },
+  {
+    "revision": "85bc2a875182f81ef23340b4cfdecd9b",
+    "url": "/react-native-elements-app/static/media/Georgia.85bc2a87.ttf"
+  },
+  {
+    "revision": "f7213526ec9296ff43426bfe3eae8926",
+    "url": "/react-native-elements-app/static/media/Montserrat-Regular.f7213526.ttf"
+  },
+  {
+    "revision": "9e52b00d737492e337b2498406aef98f",
+    "url": "/react-native-elements-app/static/media/Montserrat-Light.9e52b00d.ttf"
+  },
+  {
+    "revision": "702e5a1e12bef91a904472f93fa45b4f",
+    "url": "/react-native-elements-app/static/media/bg_screen4.702e5a1e.jpg"
+  },
+  {
+    "revision": "a8de8ffe262db7e277deb8f1c439c518",
+    "url": "/react-native-elements-app/static/media/Montserrat-Bold.a8de8ffe.ttf"
+  },
+  {
+    "revision": "219ba10ba63507cf6c59f4d360cc444f",
+    "url": "/react-native-elements-app/static/media/avatar1.219ba10b.jpg"
+  },
+  {
+    "revision": "69181674b50b43e4701f843f2ea82ba6",
+    "url": "/react-native-elements-app/static/media/bg_screen1.69181674.jpg"
+  },
+  {
+    "revision": "da34ac6ec7ab655c29821a3c055a9866",
+    "url": "/react-native-elements-app/static/media/user-cool.da34ac6e.png"
+  },
+  {
+    "revision": "98bbbe61fcc1426a22364d7aa4e4a038",
+    "url": "/react-native-elements-app/static/media/user-student.98bbbe61.png"
+  },
+  {
+    "revision": "3e4af0acbc1aabaf026aa2cf9fb8c123",
+    "url": "/react-native-elements-app/static/media/user-hp.3e4af0ac.png"
+  },
+  {
+    "revision": "7edefb07b67cd73f61b6a253d6196238",
+    "url": "/react-native-elements-app/static/media/rating.7edefb07.png"
+  },
+  {
+    "revision": "704abddf95cfc85ddd10",
+    "url": "/react-native-elements-app/static/js/runtime~main.ea2ef946.js"
+  },
+  {
+    "revision": "008e6bc48c8eaa5d2855d57e6b0b8595",
+    "url": "/react-native-elements-app/static/media/Ubuntu-Bold.008e6bc4.ttf"
+  },
+  {
+    "revision": "d8d09723b71ebb22bc31881877609622",
+    "url": "/react-native-elements-app/static/media/Ubuntu-Light-Italic.d8d09723.ttf"
+  },
+  {
+    "revision": "2798eb4f9803c2065fb023dab93628ac",
+    "url": "/react-native-elements-app/static/media/wallpaper_2.2798eb4f.jpg"
+  },
+  {
+    "revision": "299c98a65461b7202eec8db4a06bd032",
+    "url": "/react-native-elements-app/static/media/wallpaper_3.299c98a6.jpg"
+  },
+  {
+    "revision": "af7d83b2227769e05ea99e9e19b2b278",
+    "url": "/react-native-elements-app/static/media/wallpaper_4.af7d83b2.jpg"
+  },
+  {
+    "revision": "9b5178a8a761a2536322e6ca75c74240",
+    "url": "/react-native-elements-app/static/media/bg_screen2.9b5178a8.jpg"
+  },
+  {
+    "revision": "55b12e5aeb4c07cd73090d3743221981",
+    "url": "/react-native-elements-app/static/media/bg_screen3.55b12e5a.jpg"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-native-elements-app/static/media/FontAwesome.b06871f2.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/react-native-elements-app/static/media/Ionicons.b2e0fc82.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/react-native-elements-app/static/media/Entypo.744ce600.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/react-native-elements-app/static/media/SimpleLineIcons.d2285965.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/react-native-elements-app/static/media/MaterialIcons.a37b0c01.ttf"
+  },
+  {
+    "revision": "37c30c82eefa01cd2e8c",
+    "url": "/react-native-elements-app/static/js/2.b79ac22b.chunk.js"
+  },
+  {
+    "revision": "73dc6a9bf1fb21e33551514e507e34ab",
+    "url": "/react-native-elements-app/index.html"
+  }
+];

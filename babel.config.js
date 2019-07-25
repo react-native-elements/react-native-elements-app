4758254836032a3dd441e61209c50f88323f430b
Brian Dombrowski <bdombro@gmail.com>
2019-07-25 17:16:00 -0400

    Upgraded React Scripts and Babel Config

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,12 @@
 module.exports = function(api) {
   api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+
+  let presets = ['babel-preset-expo'];
+
+  let plugins = ['@babel/plugin-proposal-class-properties'];
+
+  if (process.env.platform === 'web')
+    presets.push(["@babel/preset-env", { useBuiltIns: "usage", },]);
+
+  return { presets, plugins };
 };

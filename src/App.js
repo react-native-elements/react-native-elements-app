575ef7133834093cf63f49ae3d6cd205e53c6d18
Brian Dombrowski <bdombro@gmail.com>
2019-07-25 16:01:59 -0400

    Boiled down app to skeleton

--- src/App.js
+++ src/App.js
@@ -1,83 +1,9 @@
 import React from 'react';
 import AppLoading from "./components/AppLoading";
-import { View, Image, Dimensions } from 'react-native';
-import { createAppContainer, createDrawerNavigator, DrawerItems } from 'react-navigation';
+import { View, Text } from 'react-native';
 import {cacheAssets,cacheFonts} from "./helpers/AssetsCaching";
 
-import Components from './drawer/components';
-import Ratings from './drawer/ratings';
-import Pricing from './drawer/pricing';
-import Login from './drawer/login';
-import Profile from './drawer/profile';
-import Lists from './drawer/lists';
-import Settings from './drawer/settings';
 
-const WINDOW_WIDTH = Dimensions.get('window').width;
-
-const CustomDrawerContentComponent = props => (
-  <View style={{ flex: 1, backgroundColor: '#43484d' }}>
-    <View
-      style={{ marginTop: 40, justifyContent: 'center', alignItems: 'center' }}
-    >
-      <Image
-        source={require('./images/logo.png')}
-        style={{ width: Math.min(WINDOW_WIDTH * 0.57, 200) }}
-        resizeMode="contain"
-      />
-    </View>
-    <View style={{ marginLeft: 10 }}>
-      <DrawerItems {...props} />
-    </View>
-  </View>
-);
-
-const MainRoot = createAppContainer(createDrawerNavigator(
-  {
-    Login: {
-      path: '/login',
-      screen: Login,
-    },
-    Profile: {
-      path: '/profile',
-      screen: Profile,
-    },
-    Lists: {
-      path: '/lists',
-      screen: Lists,
-    },
-    Components: {
-      path: '/components',
-      screen: Components,
-    },
-    Ratings: {
-      path: '/ratings',
-      screen: Ratings,
-    },
-    Pricing: {
-      path: '/pricing',
-      screen: Pricing,
-    },
-    Settings: {
-      path: '/settings',
-      screen: Settings,
-    },
-  },
-  {
-    initialRouteName: 'Components',
-    contentOptions: {
-      activeTintColor: '#548ff7',
-      activeBackgroundColor: 'transparent',
-      inactiveTintColor: '#ffffff',
-      inactiveBackgroundColor: 'transparent',
-      labelStyle: {
-        fontSize: 15,
-        marginLeft: 0,
-      },
-    },
-    drawerWidth: Math.min(WINDOW_WIDTH * 0.8, 300),
-    contentComponent: CustomDrawerContentComponent,
-  }
-));
 
 export default class AppContainer extends React.Component {
   state = {
@@ -86,24 +12,17 @@ export default class AppContainer extends React.Component {
 
   async _loadAssetsAsync() {
     const imageAssets = cacheAssets([
-      require("../assets/images/bg_screen1.jpg"),
-      require("../assets/images/bg_screen2.jpg"),
-      require("../assets/images/bg_screen3.jpg"),
-      require("../assets/images/bg_screen4.jpg"),
-      require("../assets/images/user-cool.png"),
-      require("../assets/images/user-hp.png"),
-      require("../assets/images/user-student.png"),
-      require("../assets/images/avatar1.jpg"),
+      // require("../assets/images/bg_screen1.jpg"),
     ]);
 
     const fontAssets = cacheFonts({
-      "FontAwesome": require("@expo/vector-icons/fonts/FontAwesome.ttf"),
-      "Ionicons": require("@expo/vector-icons/fonts/Ionicons.ttf"),
-      "Entypo": require("@expo/vector-icons/fonts/Entypo.ttf"),
-      "SimpleLineIcons": require("@expo/vector-icons/fonts/SimpleLineIcons.ttf"),
-      "MaterialIcons": require("@expo/vector-icons/fonts/MaterialIcons.ttf"),
-      //TODO: What's wrong with MaterialCommunityIcons ???
-      "MaterialCommunityIcons": require("@expo/vector-icons/fonts/MaterialCommunityIcons.ttf"),
+      // "FontAwesome": require("@expo/vector-icons/fonts/FontAwesome.ttf"),
+      // "Ionicons": require("@expo/vector-icons/fonts/Ionicons.ttf"),
+      // "Entypo": require("@expo/vector-icons/fonts/Entypo.ttf"),
+      // "SimpleLineIcons": require("@expo/vector-icons/fonts/SimpleLineIcons.ttf"),
+      // "MaterialIcons": require("@expo/vector-icons/fonts/MaterialIcons.ttf"),
+      // //TODO: What's wrong with MaterialCommunityIcons ???
+      // "MaterialCommunityIcons": require("@expo/vector-icons/fonts/MaterialCommunityIcons.ttf"),
     });
 
     await Promise.all([imageAssets, fontAssets]);
@@ -119,6 +38,6 @@ export default class AppContainer extends React.Component {
       );
     }
 
-    return <MainRoot />;
+    return <View><Text>Hello</Text></View>;
   }
 }

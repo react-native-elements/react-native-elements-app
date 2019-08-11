bbc5d81132206c1e66b5fa12b50d931d0dde7731
oxyii <oxyii.dev@gmail.com>
2019-08-11 14:16:16 +0300

    web-build PUBLIC_URL into separate app.web-build.json instead app.json

--- webpack.config.js
+++
@@ -1,7 +0,0 @@
-const createExpoWebpackConfigAsync = require('@expo/webpack-config');
-
-module.exports = async function(env, argv) {
-  const config = await createExpoWebpackConfigAsync(Object.assign(env, { polyfill: true }), argv);
-  // Customize the config before returning it.
-  return config;
-};

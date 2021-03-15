02d0f8178a49ee460fe71360282545daa1e2d76d
HariHaran <haran465@gmail.com>
2021-03-15 19:54:38 +0530

    Fixed padding vertical of loading button (#134)
    * Fixed padding vertical of loading button
    
    * replaced value

--- src/views/buttons.js
+++ src/views/buttons.js
@@ -119,7 +119,7 @@ class Buttons extends Component {
                   borderColor: 'transparent',
                   borderWidth: 0,
                   borderRadius: 5,
-                  paddingVertical: 10,
+                  paddingVertical: 5,
                 }}
                 containerStyle={{ marginTop: 20, width: 300, height: 45 }}
               />

09a2024c7ece272f139683caa7b9734f77221ee3
Arpit Bhalla <arpitbhalla2001@gmail.com>
2021-03-18 15:11:39 +0530

    Improved

--- src/views/linearProgress.tsx
+++ src/views/linearProgress.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { Header } from './header';
-import {
-  Button,
-  LinearProgress,
-  LinearProgressProps,
-} from 'react-native-elements';
+import { Button, LinearProgress } from 'react-native-elements';
 
 const LinearProgressAPI: React.FunctionComponent = () => {
   const [progress, setProgress] = React.useState<number>(0);
@@ -32,30 +28,31 @@ const LinearProgressAPI: React.FunctionComponent = () => {
           margin: 10,
         }}
       >
-        <Text>Linear Progress Indeterminate </Text>
+        <Text>Indeterminate Variant </Text>
         <LinearProgress style={{ marginVertical: 10 }} />
-        <Text>Linear Progress Secondary color</Text>
-        <LinearProgress style={{ marginVertical: 10 }} color="secondary" />
-        <Text>Linear Progress with value</Text>
+        <Text>Indeterminate Variant with color</Text>
+        <LinearProgress style={{ marginVertical: 10 }} color="red" />
+        <Text>Determinate Variant</Text>
         <LinearProgress
           style={{ marginVertical: 10 }}
           value={progress}
           variant="determinate"
         />
-        <Text>Linear Progress with value secondary color</Text>
 
-        <LinearProgress
-          style={{ marginVertical: 10 }}
-          value={progress}
-          variant="determinate"
-          color="secondary"
-        />
         <Button
           disabled={progress > 0}
           onPress={() => {
             setProgress(0.00001);
           }}
           title={'Start Progress'}
+          containerStyle={{ margin: 10 }}
+        />
+        <Button
+          disabled={progress === 0}
+          onPress={() => {
+            setProgress(0);
+          }}
+          title={'Restart'}
           containerStyle={{ margin: 10 }}
         />
       </View>

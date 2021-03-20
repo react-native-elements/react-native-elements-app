65ee7b868abb7c880c806de29a5fd2829275bf94
aashu0148 <buildforfb@gmail.com>
2021-03-20 15:20:59 +0530

    avatars.tsc component redesigned

--- src/views/avatars.tsx
+++ src/views/avatars.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { View, ScrollView } from 'react-native';
 import _ from 'lodash';
 import { Avatar } from 'react-native-elements';
-import { Header } from './header';
+import { Header, SubHeader } from './header';
 
 const dataList = [
   {
@@ -16,23 +16,27 @@ const dataList = [
     title: null,
   },
   {
-    image_url: null,
+    image_url:
+      'https://cdn.pixabay.com/photo/2019/11/03/20/11/portrait-4599553__340.jpg',
     icon: null,
-    title: 'LR',
-  },
-  {
-    image_url: null,
-    icon: { name: 'user', type: 'font-awesome' },
     title: null,
   },
   {
-    image_url: null,
-    icon: { name: 'user-female', type: 'simple-line-icon' },
+    image_url:
+      'https://cdn.pixabay.com/photo/2014/09/17/20/03/profile-449912__340.jpg',
+    icon: null,
     title: null,
   },
   {
-    image_url: null,
-    icon: { name: 'baidu', type: 'entypo' },
+    image_url:
+      'https://cdn.pixabay.com/photo/2020/09/18/05/58/lights-5580916__340.jpg',
+    icon: null,
+    title: null,
+  },
+  {
+    image_url:
+      'https://cdn.pixabay.com/photo/2016/11/22/21/42/adult-1850703__340.jpg',
+    icon: null,
     title: null,
   },
 ];
@@ -62,13 +66,13 @@ const Avatars: React.FunctionComponent<AvatarComponentProps> = () => {
     <>
       <Header title="Avatars" />
       <ScrollView>
+        <SubHeader title={'Photo Avatars'} />
         {_.chunk(dataList, 3).map((chunk, chunkIndex) => (
           <View
             style={{
               flexDirection: 'row',
               justifyContent: 'space-around',
-              marginTop: 10,
-              backgroundColor: 'grey',
+              marginBottom: 20,
             }}
             key={chunkIndex}
           >
@@ -77,45 +81,139 @@ const Avatars: React.FunctionComponent<AvatarComponentProps> = () => {
                 size={64}
                 rounded
                 source={l.image_url ? { uri: l.image_url } : null}
-                icon={l.icon}
                 title={l.title}
                 key={`${chunkIndex}-${i}`}
               />
             ))}
           </View>
         ))}
+        <SubHeader title={'Icon Avatars'} />
         <View
           style={{
             flexDirection: 'row',
             justifyContent: 'space-around',
-            marginTop: 10,
-            backgroundColor: 'grey',
+            marginBottom: 10,
           }}
         >
           <Avatar
             size={64}
             rounded
-            source={{
-              uri: 'https://randomuser.me/api/portraits/women/63.jpg',
+            icon={{ name: 'pencil', type: 'font-awesome' }}
+            containerStyle={{ backgroundColor: '#6733b9' }}
+          />
+          <Avatar
+            size={64}
+            rounded
+            icon={{ name: 'rowing' }}
+            containerStyle={{ backgroundColor: '#00a7f7' }}
+          />
+          <Avatar
+            size={64}
+            rounded
+            icon={{ name: 'heartbeat', type: 'font-awesome' }}
+            containerStyle={{ backgroundColor: '#eb1561' }}
+          />
+        </View>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-around',
+            marginBottom: 40,
+          }}
+        >
+          <Avatar
+            size={64}
+            rounded
+            icon={{
+              name: 'extension',
+              type: 'material',
+              color: '#cdde20',
             }}
+            containerStyle={{
+              borderColor: 'grey',
+              borderStyle: 'solid',
+              borderWidth: 1,
+            }}
+          />
+          <Avatar
+            size={64}
+            rounded
+            icon={{ name: 'apartment', type: 'material', color: '#009688' }}
+            containerStyle={{
+              borderColor: 'grey',
+              borderStyle: 'solid',
+              borderWidth: 1,
+            }}
+          />
+          <Avatar
+            size={64}
+            rounded
+            icon={{ name: 'backup', type: 'material', color: '#ff5606' }}
+            containerStyle={{
+              borderColor: 'grey',
+              borderStyle: 'solid',
+              borderWidth: 1,
+            }}
+          />
+        </View>
+
+        <SubHeader title={'Letter Avatars'} />
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-around',
+            marginBottom: 40,
+          }}
+        >
+          <Avatar
+            size={64}
+            rounded
+            title="Fc"
+            containerStyle={{ backgroundColor: '#3d4db7' }}
+          />
+          <Avatar
+            size={64}
+            rounded
+            title="P"
+            containerStyle={{ backgroundColor: 'coral' }}
+          />
+          <Avatar
+            size={64}
+            rounded
+            title="Rd"
+            containerStyle={{ backgroundColor: 'purple' }}
+          />
+        </View>
+
+        <SubHeader title={'Badged Avatars'} />
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-around',
+            marginBottom: 40,
+          }}
+        >
+          <Avatar
+            size={64}
+            rounded
+            icon={{ name: 'adb', type: 'material' }}
+            containerStyle={{ backgroundColor: 'orange' }}
           >
             <Avatar.Accessory size={24} />
           </Avatar>
           <Avatar
             size={64}
             rounded
-            icon={{ name: 'user', type: 'font-awesome' }}
+            source={{
+              uri:
+                'https://cdn.pixabay.com/photo/2020/06/03/14/53/girl-5255195__340.jpg',
+            }}
+            title="Bj"
+            containerStyle={{ backgroundColor: 'grey' }}
           >
             <Avatar.Accessory size={23} />
           </Avatar>
         </View>
-        <View
-          style={{
-            flexDirection: 'row',
-            marginTop: 10,
-            backgroundColor: 'grey',
-          }}
-        />
       </ScrollView>
     </>
   );

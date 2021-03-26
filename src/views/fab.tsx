9d8548aa761ad4211432dfc3cbbc10571ab305b0
Arpit Bhalla <arpitbhalla2001@gmail.com>
2021-03-26 11:23:19 +0530

    Fixed FAB

--- src/views/fab.tsx
+++ src/views/fab.tsx
@@ -16,35 +16,39 @@ export default () => {
           flexGrow: 1,
         }}
       >
-        <Text style={{color: '#397af8', paddingVertical: 10}}>Small Size</Text>
+        <Text style={{ color: '#397af8', paddingVertical: 10 }}>
+          Small Size
+        </Text>
         <FAB
           loading
           visible={visible}
           icon={{ name: 'add', color: 'white' }}
           size="small"
         />
-        <Text style={{color: '#397af8', paddingVertical: 10}}>Large Size</Text>
+        <Text style={{ color: '#397af8', paddingVertical: 10 }}>
+          Large Size
+        </Text>
         <FAB
           visible={visible}
           icon={{ name: 'add', color: 'white' }}
           color="blue"
         />
-        <Text style={{color: '#397af8', paddingVertical: 10}}>Primary Color</Text>
+        <Text style={{ color: '#397af8', paddingVertical: 10 }}>
+          Primary Color
+        </Text>
         <FAB
           visible={visible}
-          label="Navigate"
+          title="Navigate"
           upperCase
           icon={{ name: 'place', color: 'white' }}
         />
 
-<Text style={{color: '#397af8', paddingVertical: 10}}>Disabled</Text>
-
- 
+        <Text style={{ color: '#397af8', paddingVertical: 10 }}>Disabled</Text>
 
         <FAB
           visible={visible}
           disabled
-          label="Extended"
+          title="Extended"
           icon={{
             name: 'place',
             color: 'white',
@@ -54,7 +58,7 @@ export default () => {
           visible={visible}
           onPress={() => setVisible(!visible)}
           placement="right"
-          label="Hide"
+          title="Hide"
           icon={{ name: 'delete', color: 'white' }}
           color="blue"
         />
@@ -62,7 +66,7 @@ export default () => {
           visible={!visible}
           onPress={() => setVisible(!visible)}
           placement="left"
-          label="Show"
+          title="Show"
           icon={{ name: 'edit', color: 'white' }}
           color="green"
         />

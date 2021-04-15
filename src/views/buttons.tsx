7901617137ebe3bee2c3f03a155863d36867ed0b
Dhananjayan P N <pndjay@gmail.com>
2021-04-15 16:24:29 +0530

    update props

--- src/views/buttons.tsx
+++ src/views/buttons.tsx
@@ -252,7 +252,7 @@ const Buttons: React.FunctionComponent<ButtonsComponentProps> = () => {
                   borderColor: 'rgba(78, 116, 289, 1)',
                 }}
                 type="outline"
-                raised="true"
+                raised
                 titleStyle={{ color: 'rgba(78, 116, 289, 1)' }}
                 containerStyle={{
                   width: 200,
